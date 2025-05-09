4a2189d3ccc5b260dc4c13cc12fcb31c0194ac13
nasamio <nasamio@163.com>
2025-05-09 13:53:40 +0800

    1.更新桌面端icon

--- composeApp/src/desktopMain/kotlin/com/mio/main.kt
+++ composeApp/src/desktopMain/kotlin/com/mio/main.kt
@@ -2,11 +2,15 @@ package com.mio
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import netmusickmp.composeapp.generated.resources.Res
+import netmusickmp.composeapp.generated.resources.ic_app
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "NetMusicKmp",
+        icon = painterResource(Res.drawable.ic_app)
     ) {
         App()
     }
