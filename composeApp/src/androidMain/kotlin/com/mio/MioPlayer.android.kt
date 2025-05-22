f4ad65196d4baeadbac2cd450ebd79358bb79f4f
nqcpn <nqccpn@163.com>
2025-05-22 14:36:08 +0800

    1. MioPlayer桌面，使用vlcj实现

---
+++ composeApp/src/androidMain/kotlin/com/mio/MioPlayer.android.kt
@@ -0,0 +1,21 @@
+package com.mio
+
+actual object MioPlayer {
+
+    actual fun init() {
+
+    }
+
+    actual fun play(url: String) {
+
+    }
+
+    actual fun pause() {}
+    actual fun resume() {}
+    actual fun stop() {}
+
+    actual fun release() {
+
+    }
+
+}
